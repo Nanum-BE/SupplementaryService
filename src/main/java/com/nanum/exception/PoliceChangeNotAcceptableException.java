14b86e7f3784dfc2fdeadd29b149762520648cf9
chan park <46785614+fineman999@users.noreply.github.com>
2022-09-30 19:43:55 +0900

    [#7] feat: api update

--- src/main/java/com/nanum/exception/PoliceChangeNotAcceptableException.java
+++ src/main/java/com/nanum/exception/PoliceChangeNotAcceptableException.java
@@ -1,5 +1,9 @@
 package com.nanum.exception;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.NOT_FOUND)
 public class PoliceChangeNotAcceptableException extends RuntimeException {
     public PoliceChangeNotAcceptableException(String format) {
         super(format);
